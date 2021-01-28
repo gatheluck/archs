eba6a61c55bac9ed0bdde0933a47d8b1e647e07e
gatheluck <gatheluck+tech@gmail.com>
2021-01-28 19:08:11 +0900

    style: apply black, flake8, isort.

--- archs/__init__.py
+++ archs/__init__.py
@@ -1,2 +1,3 @@
-from .core.wideresnet import wideresnet16, wideresnet28, wideresnet40
-from .core.resnet import resnet20, resnet32, resnet44, resnet56, resnet110, resnet1202
+from .core.resnet import (resnet20, resnet32, resnet44, resnet56,  # noqa
+                          resnet110, resnet1202)
+from .core.wideresnet import wideresnet16, wideresnet28, wideresnet40  # noqa
