ebf003ccc13ff06cf450c1c6548ce9f7336e41f5
gatheluck <gatheluck+tech@gmail.com>
2021-01-28 21:55:15 +0900

    refactor: remove redundant import.

--- archs/__init__.py
+++ archs/__init__.py
@@ -1,3 +1,2 @@
-from .core.resnet import (resnet20, resnet32, resnet44, resnet56,  # noqa
-                          resnet110, resnet1202)
+from .core.resnet import resnet20, resnet32, resnet44, resnet56  # noqa
 from .core.wideresnet import wideresnet16, wideresnet28, wideresnet40  # noqa
