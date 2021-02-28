0f499ddef2b1c794f7d2785bdcda0b3435d5cf54
gatheluck <gatheluck+tech@gmail.com>
2021-02-28 22:31:43 +0900

    feat: add deit model.

--- archs/core/vit.py
+++ archs/core/vit.py
@@ -55,3 +55,30 @@ def vit_large16(pretrained: bool = False, num_classes: int = 1000) -> nn.Module:
     return timm.create_model(
         "vit_large_patch16_224", pretrained=pretrained, num_classes=num_classes
     )
+
+
+def deit_base16(pretrained: bool = False, num_classes: int = 1000, distilled: bool = False):
+    """DeiT-Base (DeiT-B/16).
+
+    This function just call function in timm repo https://github.com/rwightman/pytorch-image-models (Apache License 2.0).
+
+    Note:
+        The main params to create Vision Transformer is shown in below.
+        - patch_size = 16
+        - embed_dim = 768
+        - depth = 12
+        - num_heads = 12
+
+    Args:
+        pretrained (bool): If pretrained is True, this function downloads and loads pretrained model of ImageNet-1k.
+        num_classes (int): The number of class.
+        distilled: (bool): If True, use distilled model.
+
+    Returns:
+        nn.Module: DeiT model.
+
+    """
+    if distilled:
+        return timm.create_model("vit_deit_base_distilled_patch16_224", pretrained=pretrained, num_classes=num_classes)
+    else:
+        return timm.create_model("vit_deit_base_patch16_224", pretrained=pretrained, num_classes=num_classes)
